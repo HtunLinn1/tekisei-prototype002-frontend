8a7b7c6967729bf1e7e833c9290dde3259bb36e9
HtunLinn <htunlinn@mm.cybermissions.co.jp>
2022-04-14 18:56:58 +0900

    fix

--- components/CompleteForm.vue
+++ components/CompleteForm.vue
@@ -1,23 +1,29 @@
 <template>
   <v-container class="text-center">
     <p v-if="answered_index === -1">
+      Part1の問題はいじょうです。<br>
       終了してよろしいですか
     </p>
     <p v-else>
-      未回答の問題があります
+      未回答の問題があります。
     </p>
-    <div v-for="(ans) in selected_answers" :key="ans.qusId">
-      <v-chip
-        class="ma-2 chip"
-        :color="ans.answer === ''? '#E9967A' : '#82E0AA'"
-        text-color="white"
-        width="200"
-        @click="clickQusId(ans.onboarding)"
-      >
-        {{ ans.qusId }} .&nbsp;
-        {{ ans.answer }}
-      </v-chip>
-      <span v-if="ans.checkbox" class="star">*</span>
+    <v-row>
+      <v-col v-for="(ans) in selected_answers" :key="ans.qusId" cols="4">
+        <v-chip
+          class="ma-2 chip"
+          :color="ans.answer === ''? '#E9967A' : '#82E0AA'"
+          text-color="white"
+          width="200"
+          @click="clickQusId(ans.onboarding)"
+        >
+          {{ ans.qusId }} .&nbsp;
+          {{ ans.answer }}
+        </v-chip>
+        <span v-if="ans.checkbox" class="star">*</span>
+      </v-col>
+    </v-row>
+    <div >
+
     </div>
   </v-container>
 </template>
