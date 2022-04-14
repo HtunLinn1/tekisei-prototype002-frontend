d8ef0bf2a43afd914efa0396de460736ad87202b
HtunLinn <htunlinn@mm.cybermissions.co.jp>
2022-04-14 18:23:13 +0900

    explain1

--- components/Part1Explain.vue
+++ components/Part1Explain.vue
@@ -1,68 +1,129 @@
 <template>
   <v-container>
-    <v-row v-if="!isStartTest">
-      <v-col>
-        <v-carousel
-          v-model="slide"
-          cycle
-          height="400"
-          hide-delimiter-background
-        >
-          <template #prev="{ on, attrs }">
-            <v-btn
-              color="info"
-              icon
-              v-bind="attrs"
-              v-on="on"
-            >
-              <v-icon>
-                mdi-chevron-left
-              </v-icon>
-            </v-btn>
-          </template>
-          <template #next="{ on, attrs }">
-            <v-btn
-              v-if="slide + 1 < slides.length"
-              color="info"
-              icon
-              v-bind="attrs"
-              v-on="on"
-            >
-              <v-icon>
-                mdi-chevron-right
-              </v-icon>
-            </v-btn>
-            <v-btn
-              v-if="slide + 1 === slides.length"
-              color="info"
-              icon
-              @click="startTest"
-            >
-              開始
-            </v-btn>
-          </template>
-          <v-carousel-item
-            v-for="(slide, i) in slides"
-            :key="i"
+    <div v-if="!isStartTest" class="text-center">
+      <v-row>
+        <v-col class="text-h4">
+          第Ⅰ部
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-h5">
+          制限時間 ： 25分
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-h6">
+          第Ⅰ部には以下のような4種類の問題があります。<br>
+          それぞれの問題について5択より選択し解答してください。
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-carousel
+            v-model="slide"
+            cycle
+            height="400"
+            hide-delimiter-background
           >
-            <v-sheet
-              :color="colors[i]"
-              height="100%"
-            >
-              <v-row
-                class="fill-height"
-                align="center"
-                justify="center"
+            <template #prev="{ on, attrs }">
+              <v-btn
+                color="blue"
+                icon
+                large
+                :disabled="slide === 0"
+                v-bind="attrs"
+                v-on="on"
               >
-                <div class="text-h2">
-                  {{ slide }} Slide
-                </div>
-              </v-row>
-            </v-sheet>
-          </v-carousel-item>
-        </v-carousel>
-      </v-col>
-    </v-row>
+                <v-icon>
+                  mdi-chevron-left
+                </v-icon>
+              </v-btn>
+            </template>
+            <template #next="{ on, attrs }">
+              <v-btn
+                v-if="slide + 1 < questionList.length"
+                color="blue"
+                icon
+                large
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>
+                  mdi-chevron-right
+                </v-icon>
+              </v-btn>
+              <v-btn
+                v-if="slide + 1 === questionList.length"
+                color="primary"
+                icon
+                large
+                @click="startTest"
+              >
+                <span style="color: blue;">開始</span>
+              </v-btn>
+            </template>
+            <v-carousel-item
+              v-for="(qus, i) in questionList"
+              :key="i"
+            >
+              <v-sheet
+                height="100%"
+              >
+                <v-row>
+                  <v-col class="text-body1">
+                    {{ i + 1 }}.{{ qus.question_description }}
+                  </v-col>
+                </v-row>
+                <v-row v-if="'question' in qus">
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="6"
+                    class="text-h6"
+                  >
+                    {{ qus.question }}
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="6"
+                    class="text-body1"
+                  >
+                    <v-row class="text-h6 ml-8">
+                      <v-col v-for="(ans, i) in qus.answer" :key="i">
+                        ({{ i + 1 }})&nbsp;<br>
+                        {{ ans }}
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+                <v-row v-else>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="6"
+                  >
+                    <v-img :src="require(`~/assets/images/${qus.question_img}`)" />
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="6"
+                  >
+                    <v-img :src="require(`~/assets/images/${qus.answer_img}`)" />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="text-body2">
+                    {{ qus.answer_description }}
+                  </v-col>
+                </v-row>
+              </v-sheet>
+            </v-carousel-item>
+          </v-carousel>
+        </v-col>
+      </v-row>
+    </div>
     <v-row v-if="isStartTest" justify="center" align="center" class="pt-7">
       <v-col cols="12">
         <Part1QuestionList @testFinish="testFinish($event)" />
@@ -72,39 +133,27 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        slide: 0,
-        isStartTest: false,
-        colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-      }
+import questions from '@/assets/data/part1Explain.json'
+export default {
+  data () {
+    return {
+      questionList: questions,
+      slide: 0,
+      isStartTest: false,
+    }
+  },
+  created() {
+    this.isStartTest = localStorage.getItem("is-start-test")
+  },
+  methods: {
+    startTest () {
+      this.isStartTest = true
+      localStorage.setItem("is-start-test", this.isStartTest)
     },
-    created() {
-      this.isStartTest = localStorage.getItem("is-start-test")
-    },
-    methods: {
-      startTest () {
-        this.isStartTest = true
-        localStorage.setItem("is-start-test", this.isStartTest)
-      },
-      testFinish(status) {
-        localStorage.removeItem("is-start-test")
-        this.$emit('testFinish', { part: status.part })
-      }
-    },
-  }
+    testFinish(status) {
+      localStorage.removeItem("is-start-test")
+      this.$emit('testFinish', { part: status.part })
+    }
+  },
+}
 </script>
